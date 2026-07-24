9a5c2d8f2b2a8404b632167ad59dc5ad729d37b5
man3ourk <erore9035@gmail.com>
2026-07-24 23:34:57 +0330

    Add comments for layout instructions in Persian
    Added comments in Persian for layout instructions.

--- app/src/main/java/com/example/jetbot/MainActivity.kt
+++ app/src/main/java/com/example/jetbot/MainActivity.kt
@@ -6,5 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // اگر layout نداری فعلاً همین کافی است
+        // setContentView(R.layout.activity_main)
     }
 }
