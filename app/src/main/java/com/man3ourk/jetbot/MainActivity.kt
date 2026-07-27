60a8a39a4472a5406399cd28bbb35d0ec4821186
man3ourk <erore9035@gmail.com>
2026-07-27 20:11:39 +0330

    Update MainActivity.kt

--- app/src/main/java/com/man3ourk/jetbot/MainActivity.kt
+++ app/src/main/java/com/man3ourk/jetbot/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.man3ourk.jetbot
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textStatus = findViewById<TextView>(R.id.textStatus)
+        textStatus.text = getString(R.string.app_ready)
     }
 }
