ca179d7928c433d7a76169eb2b0d9938859e30d3
man3ourk <erore9035@gmail.com>
2026-07-28 00:12:28 +0330

    Add Gradle Wrapper files to fix build failure

--- gradlew.bat
+++ gradlew.bat
@@ -1,3 +1,64 @@
-@echo off
-set DIR=%~dp0
-java -classpath "%DIR%gradle\\wrapper\\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
+@rem Gradle startup script for Windows
+@rem In order to run Gradle from DOS prompt you need to do the following:
+@rem
+@rem    - Download Gradle from https://gradle.org/downloads
+@rem    - Unzip it to your work directory
+@rem    - Update PATH to include the bin directory of the unzipped Gradle distribution
+@rem    - In Eclipse, go to Window -> Preferences -> Java -> Installed JREs -> add your JRE
+@rem    - In IntelliJ, go to File -> Project Structure -> Project -> SDK and select the JRE
+
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem  Gradle startup script for Windows
+@rem ##########################################################################
+
+setlocal enabledelayedexpansion
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_HOME=%DIRNAME%
+
+@rem Resolve any "."
+cd /d "%APP_HOME%"
+
+@rem Find java.exe
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >nul 2>&1
+if %ERRORLEVEL% equ 0 goto execute
+
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+
+if exist "%JAVA_EXE%" goto execute
+
+echo.
+echo ERROR: JAVA_HOME is not set and a 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
+
+:execute
+@rem Setup the command line
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+
+:fail
+exit /b 1
+
+:omega
